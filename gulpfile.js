8bff3d04ff13005fdd74ab6bcd2b321f2e32cd69
naycho334 <elkhaouti@gmail.com>
2022-08-30 11:03:26 +0100

    fix css prefixer + fix js transpiler + add support to typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const {
   readFileSync,
@@ -7,63 +7,50 @@ const {
   rmdirSync,
   rmSync,
 } = require("fs");
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer');
-const source = require('vinyl-source-stream');
-const browserify = require('browserify');
-const { copySync } = require('fs-extra');
-const { zip } = require('zip-a-folder');
-const buffer = require('vinyl-buffer');
+const sass = require("gulp-sass")(require("sass"));
+const prefixer = require("gulp-autoprefixer");
+const source = require("vinyl-source-stream");
+const browserify = require("browserify");
+const { copySync } = require("fs-extra");
+const { zip } = require("zip-a-folder");
+const buffer = require("vinyl-buffer");
 const minify = require("gulp-minify");
-const Babelify = require('babelify');
-const { series } = require('gulp');
+const Babelify = require("babelify");
+const { series } = require("gulp");
 const { join } = require("path");
-const gulp = require('gulp');
-const os = require('os');
+const tsify = require("tsify");
+const gulp = require("gulp");
+const os = require("os");
 
 const compileSCSS = (event) => {
-  gulp.src('./assets/scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+  gulp
+    .src("./assets/scss/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(prefixer("last 2 versions"))
+    .pipe(gulp.dest("./assets/css"));
 
   event !== undefined && event();
-}
+};
 
-const autoPrefixCss = (event) => {
-  gulp.src('./assets/css/**/*.css')
-    .pipe(autoprefixer({
-      cascade: false,
-      flexbox: true,
-      supports: true,
-    }))
-    .pipe(gulp.dest('./assets/css'));
-
-  event !== undefined && event();
-}
-
-const removeFiles = (path = '', ext = '') => {
+const removeFiles = (path = "", ext = "") => {
   readdirSync(path)
-    .filter(file => file.endsWith(ext))
-    .forEach(file => rmSync(join(path, file)));
-}
+    .filter((file) => file.endsWith(ext))
+    .forEach((file) => rmSync(join(path, file)));
+};
 
 const removeCssFiles = (event) => {
-  removeFiles(join(__dirname, 'assets', 'css'), 'css');
+  removeFiles(join(__dirname, "assets", "css"), "css");
 
   event !== undefined && event();
-}
+};
 
 /**
  * Watch scss
  */
-gulp.task('watchscss', function(){
+gulp.task("watchscss", function () {
   removeCssFiles();
   compileSCSS();
-  gulp.watch(['./assets/scss/*.scss'], series(
-    removeCssFiles,
-    compileSCSS,
-    autoPrefixCss
-  ));
+  gulp.watch(["./assets/scss/*.scss"], series(removeCssFiles, compileSCSS));
 });
 
 /**
@@ -109,19 +96,57 @@ gulp.task("pack", async function () {
  * Complile and minify js files
  */
 gulp.task("compilejs", function () {
-  return browserify([join(__dirname, "assets", "js", "src", "app.js")])
-    .transform(Babelify, {})
+  return browserify([join(__dirname, "assets", "js", "src", "main.js")])
+    .transform(Babelify, {
+      presets: ["@babel/preset-react"],
+      plugins: [
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-transform-classes",
+        "@babel/plugin-transform-modules-commonjs",
+      ],
+    })
     .bundle()
     .pipe(source("app.js"))
     .pipe(buffer())
-    .pipe(minify())
+    .pipe(
+      minify({
+        ext: { min: ".min.js" },
+      })
+    )
     .pipe(gulp.dest(join(__dirname, "assets", "js", "dist")));
 });
 
 // watch and run compilejs task
 gulp.task("watchjs", function () {
-  gulp.watch(
-    [join(__dirname, "assets", "js", "src", "app.js")],
-    series("compilejs")
-  );
+  gulp.watch([join(__dirname, "assets", "js", "src")], series("compilejs"));
+});
+
+/**
+ * Complile and minify js files
+ */
+gulp.task("compilets", function () {
+  return browserify({
+    basedir: ".",
+    debug: true,
+    entries: ["assets/js/src/main.ts"],
+    cache: {},
+    packageCache: {},
+  })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source("app.js"))
+    .pipe(buffer())
+    .pipe(
+      minify({
+        ext: {
+          min: ".min.js",
+        },
+      })
+    )
+    .pipe(gulp.dest(join(__dirname, "assets", "js", "dist")));
+});
+
+// watch and run compilets task
+gulp.task("watchts", function () {
+  gulp.watch([join(__dirname, "assets", "js", "src")], series("compilets"));
 });
