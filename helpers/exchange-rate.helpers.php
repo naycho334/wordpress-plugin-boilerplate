227c18fee25a4d6a6b30452b993edabcb745b8ee
naycho334 <elkhaouti@gmail.com>
2022-09-24 08:38:59 +0100

    fix minor issues

--- helpers/exchange-rate.helpers.php
+++ helpers/exchange-rate.helpers.php
@@ -1,62 +1,67 @@
 <?php
 
-/**
- * Get exchange rate for a currency
- * 
- * @param string $base_currency Base currency
- * @param string $target_currency Target currency
- * @return float
- */
-function nc_get_exchange_rate($base_currency, $target_currency)
-{
-  // check if cached in the memory
-  $exchange_rate = wp_cache_get('nc_exchange_rate');
+if (!function_exists('nc_get_exchange_rate')) {
 
-  if ($exchange_rate !== false) {
+  /**
+   * Get exchange rate for a currency
+   * 
+   * @param string $base_currency Base currency
+   * @param string $target_currency Target currency
+   * @return float
+   */
+  function nc_get_exchange_rate($base_currency, $target_currency)
+  {
+    // check if cached in the memory
+    $exchange_rate = wp_cache_get('nc_exchange_rate');
+
+    if ($exchange_rate !== false) {
+      return $exchange_rate;
+    }
+
+    // check if cached in the database
+    $exchange_rate = get_transient('nc_exchange_rate');
+
+    if ($exchange_rate) {
+      return $exchange_rate;
+    }
+
+    $exchange_rate = 0;
+    $base_currency = strtoupper($base_currency);
+    $target_currency = strtoupper($target_currency);
+
+    $url = "https://api.exchangerate.host/latest?base={$base_currency}&symbols={$target_currency}";
+    $response = wp_remote_get($url);
+    $body = wp_remote_retrieve_body($response);
+    $data = json_decode($body, true);
+
+    if (isset($data['rates'][$target_currency])) {
+      $exchange_rate = $data['rates'][$target_currency];
+    }
+
+    // cache in the memory
+    wp_cache_set('nc_exchange_rate', $exchange_rate);
+
+    // cache in the database
+    set_transient('nc_exchange_rate', $exchange_rate, HOUR_IN_SECONDS);
+
     return $exchange_rate;
   }
-
-  // check if cached in the database
-  $exchange_rate = get_transient('nc_exchange_rate');
-
-  if ($exchange_rate) {
-    return $exchange_rate;
-  }
-
-  $exchange_rate = 0;
-  $base_currency = strtoupper($base_currency);
-  $target_currency = strtoupper($target_currency);
-
-  $url = "https://api.exchangerate.host/latest?base={$base_currency}&symbols={$target_currency}";
-  $response = wp_remote_get($url);
-  $body = wp_remote_retrieve_body($response);
-  $data = json_decode($body, true);
-
-  if (isset($data['rates'][$target_currency])) {
-    $exchange_rate = $data['rates'][$target_currency];
-  }
-
-  // cache in the memory
-  wp_cache_set('nc_exchange_rate', $exchange_rate);
-
-  // cache in the database
-  set_transient('nc_exchange_rate', $exchange_rate, HOUR_IN_SECONDS);
-
-  return $exchange_rate;
 }
 
-/**
- * Convert price to a target currency
- * 
- * @param float $price Price
- * @param string $base_currency Base currency
- * @param string $target_currency Target currency
- * 
- * @return float
- */
-function nc_convert_price($price, $base_currency, $target_currency)
-{
-  $exchange_rate = nc_get_exchange_rate($base_currency, $target_currency);
+if (!function_exists('nc_convert_price')) {
+  /**
+   * Convert price to a target currency
+   * 
+   * @param float $price Price
+   * @param string $base_currency Base currency
+   * @param string $target_currency Target currency
+   * 
+   * @return float
+   */
+  function nc_convert_price($price, $base_currency, $target_currency)
+  {
+    $exchange_rate = nc_get_exchange_rate($base_currency, $target_currency);
 
-  return $price * $exchange_rate;
+    return $price * $exchange_rate;
+  }
 }
--- helpers/geo-api.helpers.php
+++ helpers/geo-api.helpers.php
@@ -1,41 +1,44 @@
 <?php
 
-/**
- * Get country by IP address
- * @return object|null
- */
-function nc_get_country_by_ip_address()
-{
-  $ip = $_SERVER['REMOTE_ADDR'];
-  $key = "nc_user_ip_{$ip}";
+if (!function_exists('nc_get_country_by_ip_address')) {
 
-  $country = null;
+  /**
+   * Get country by IP address
+   * @return object|null
+   */
+  function nc_get_country_by_ip_address()
+  {
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $key = "nc_user_ip_{$ip}";
 
-  // check if ip is stored in memory
-  $ip_memory = wp_cache_get($key);
+    $country = null;
 
-  if ($ip_memory !== false) {
-    $country = $ip_memory;
-  } else {
-    // check if ip is stored in cache
-    $ip_cache = get_transient($key);
+    // check if ip is stored in memory
+    $ip_memory = wp_cache_get($key);
 
-    if ($ip_cache) {
-      $country = $ip_cache;
+    if ($ip_memory !== false) {
+      $country = $ip_memory;
     } else {
-      $ip_data = @json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
+      // check if ip is stored in cache
+      $ip_cache = get_transient($key);
 
-      if ($ip_data && gettype($ip_data) === 'object') {
-        $country = $ip_data;
+      if ($ip_cache) {
+        $country = $ip_cache;
+      } else {
+        $ip_data = @json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
+
+        if ($ip_data && gettype($ip_data) === 'object') {
+          $country = $ip_data;
+        }
+
+        // store ip in cache for 1 day
+        set_transient($key, $country, 60 * 60 * 24);
       }
 
-      // store ip in cache for 1 day
-      set_transient($key, $country, 60 * 60 * 24);
+      // store ip in memory
+      wp_cache_set($key, $country ? "yes" : "no");
     }
 
-    // store ip in memory
-    wp_cache_set($key, $country ? "yes" : "no");
+    return $country;
   }
-
-  return $country;
 }
